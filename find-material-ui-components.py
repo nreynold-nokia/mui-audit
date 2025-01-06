ae90ddc3025cd52e6090619d2383bfa8206692d2
Nolan Reynolds <nolan.reynolds@nokia.com>
2025-01-06 15:48:03 -0500

    merge changes for csv

--- find-material-ui-components.py
+++ find-material-ui-components.py
@@ -3,13 +3,28 @@ import os
 import re
 import json
 import argparse
+import csv
+from pathlib import PurePath
 
 # output
-output_file="mui-audit.json"
+output_file_json="mui-audit.json"
+output_file_csv = "mui-audit.csv"
 
 import_pattern = re.compile(
     r"import\s+(\{[^\}]*\}|[a-zA-Z0-0_]+)\s+from\s+['\"]@material-ui"
 )
+style_guide_import_pattern = re.compile(r"import\s+(\{[^\}]*\}|[a-zA-Z0-0_]+)\s+from\s+['\"]style-guide/")
+mui5_alternatives = [
+    "skeleton",
+    "list",
+    "menuitem",
+    "grey",
+    "accordion",
+    "listitem",
+    "listitemtext",
+    "listitemicon",
+    "grid",
+]
 
 def extract_components_from_import(line):
     line = re.sub(r"\s+", " ", line)
@@ -21,9 +36,10 @@ def extract_components_from_import(line):
 
 
 def search_files_for_imports(directory):
+    scan_dir = args.directory + "/ui/src"
     results = {}
     component_counts = defaultdict(int) # track frequency of components
-    for root, _, files in os.walk(directory):
+    for root, _, files in os.walk(scan_dir):
         for file in files:
             if file.endswith((".js", ".mjs", ".jsx", ".ts", ".tsx")):
                 full_path = os.path.join(root, file)
@@ -32,16 +48,28 @@ def search_files_for_imports(directory):
 
                 # Search import patterns
                 matches = import_pattern.finditer(content)
-                file_path = full_path.split('deepfield-bootstrap/')[1]
+                path_obj = PurePath(full_path)
+                file_path = path_obj.relative_to(directory).parent.__fspath__()
+                file_name = path_obj.name
+                pd_path = "pipedream/" + file_path + "/" + file_name
                 components = []
                 for match in matches:
                     components.extend(extract_components_from_import(match.group(0)))
                 if components:
                     # remove duplicates and sort
                     unique_components = sorted(set(components))
-                    results[file_path] = {
+                    # check style guide 
+                    has_style_guide = (
+                        False
+                        if style_guide_import_pattern.search(content) is None
+                        else True
+                    )
+                    results[pd_path] = {
                         "imports":unique_components,
-                        "count": len(unique_components)
+                        "name": file_name,
+                        "path": pd_path,
+                        "count": len(unique_components),
+                        "has_style_guide": has_style_guide
                     }
                     for component in unique_components:
                         component_counts[component] += 1
@@ -64,22 +92,44 @@ def write_results_to_json(results, metadata, output_file):
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
+def write_results_to_csv(results, output_file):
+    with open(output_file, "w", newline="") as csvfile:
+        cwriter = csv.writer(csvfile, delimiter=",")
+        cwriter.writerow(
+            [
+                "Parent Path",
+                "File Name",
+                "Exists in Webb UI",
+                "MUI 5",
+                "Includes Style Guide",
+            ]
+        )
+        for _, elements in results.items():
+            components = elements["imports"]
+            file_name = elements["name"]
+            path_to_track = elements["path"]
+            has_style_guide = elements["has_style_guide"]
 
-def write_results_to_txt_file(results, output_file):
-    
-    with open(output_file, "w", encoding="utf-8") as f:
-        for file, components in results.items():
-            f.write(f" {file}\n")
-            for component in set(components):
-                f.write(f" {component}\n")
-
-            f.write("\n")
+            webb_list = []
+            mui_list = []
+            # evaluate which components do not exist in webb ui
+            for s in set(components):
+                if s.lower() in mui5_alternatives:
+                    mui_list.append(s)
+                else:
+                    webb_list.append(s)
+            webb_list = "\n".join(webb_list)
+            mui_list = "\n".join(mui_list)
+            cwriter.writerow(
+                [path_to_track, file_name, webb_list, mui_list, has_style_guide]
+            )
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Scan for Material UI imports in a directory")
-    parser.add_argument("directory", type=str, help="The directory to search for imports")
+    parser.add_argument("directory", type=str, help="Your pipedream directory")
     args = parser.parse_args()
     results, metadata = search_files_for_imports(args.directory)
-    write_results_to_json(results, metadata, output_file)
+    write_results_to_json(results, metadata, output_file_json)
+    write_results_to_csv(results, output_file_csv)
